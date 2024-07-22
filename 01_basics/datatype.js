943776a88a4f4f5e4f9a1d5a9a89349fa96f0753
tanmaywithin <162417888+tanmaywithin@users.noreply.github.com>
2024-07-22 14:16:13 +0000

    lezarned datatypes in javascript

---
+++ 01_basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js code as newer  version
+
+//alert(3 + 3)//we are using node js , not browser
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//bolllean=> true/false
+//null=> standalone value
+//undefined
+//symbol=>unique
+
+
+//object
+console.log(typeof"tanmay" )//string
+console.log(typeof undefined ) //undefined
+console.log(typeof null )//object
